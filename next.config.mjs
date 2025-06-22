a47226ca2b148171b4466a6d340e0041b343d849
Kemar Taylor <kemartaylor@macbookair.home>
2025-06-22 17:08:58 +0100

    content policy update to trust iframe helper

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-// A more secure and comprehensive Content Security Policy
+// A secure and comprehensive Content Security Policy
 const ContentSecurityPolicy = `
   default-src 'self';
   script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/ https://apis.google.com;
   style-src 'self' 'unsafe-inline';
   img-src * 'self' data:;
   font-src 'self' data:;
-  frame-src 'self' https://www.google.com/recaptcha/ https://recaptcha.google.com/;
+  frame-src 'self' https://www.google.com/recaptcha/ https://recaptcha.google.com/ https://*.firebaseapp.com;
   connect-src 'self' https://*.googleapis.com https://*.firebaseapp.com;
 `;
 
@@ -23,10 +23,12 @@ const nextConfig = {
   async headers() {
     return [
       {
+        // Apply these headers to all routes in your application.
         source: '/:path*',
         headers: [
           {
             key: 'Content-Security-Policy',
+            // We remove extra whitespace from the CSP string for the header.
             value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
           },
         ],
